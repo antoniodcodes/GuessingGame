5930fd6e47188ad124a83dbaf23be66c65a74ab7
Antonio Dinkins <antonio.dinkins2019@gmail.com>
2025-01-26 00:52:29 -0500

    Added definition for get_user_name function

--- game.py
+++ game.py
@@ -13,7 +13,36 @@ def get_user_name():
   username = input("What's your name? ")
   return username
 
+def check_guess(guess, choice):
+  if guess > choice:
+    return 1
+  elif guess < choice:
+    return -1
+  else:
+    return 0
+  
+
 def main():
-  pass
+  choice = randint(1, 101)
+  greeting()
+  username = get_user_name()
+  sentinel = input("Would you like tp play a game? (yes/no) ").lower()
+  try:
+    while sentinel == 'yes' or sentinel == 'y':
+      guess = int(input("What's your guess? "))
+      number_of_guesses += 1
+      result = check_guess(guess, choice)
+      if result == -1:
+        print("Your guess was too low.")
+      elif result == 1:
+        print("Your guess was too high.")
+      else:
+        print(f"You guessed correctly, {username}. You completed it in {number_of_guesses} guesses.")
+        sentinel = input("Would you like to play again? (yes/no) ").lower()
+  except ValueError:
+    print("Please provide a valid number. ")
+    main()
+
+
 
 main()
